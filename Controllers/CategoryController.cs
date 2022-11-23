d36c0022ecb3b9c08868f97d859995da775ff95a
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-11-23 16:55:06 +0100

    mi manca la delete

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,17 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.Controllers
 {
     public class CategoryController : Controller
     {
+        PizzaDbContext Db { get; set; }
+
+        public CategoryController()
+        {
+            Db = new PizzaDbContext();
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = Db.Categories.ToList();   
+            return View(categories);
         }
 
         public IActionResult Create()
         {
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+
+            if (!ModelState.IsValid)
+                return View();
+
+            Db.Categories.Add(category);
+            Db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category category = Db.Categories.Where(cat => cat.Id == id).FirstOrDefault();
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            category.Id = id;
+
+            if (!ModelState.IsValid)
+                return View();
+
+            Db.Update(category);
+            Db.SaveChanges();
+            
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,12 +1,15 @@
-﻿namespace la_mia_pizzeria_static.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
 {
     public class Category
     {
         public int Id { get; set; }
 
+        [StringLength(50, ErrorMessage = "Non può essere più lungo di 50 caratteri")]
         public string Title { get; set; }
 
-        public List<Pizza> Pizzas { get; set; }
+        public List<Pizza>? Pizzas { get; set; }
 
 
     }
