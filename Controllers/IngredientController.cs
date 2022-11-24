9cf93974144db11cfe3d9e43d228fdfc7e119570
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-11-24 17:26:48 +0100

    CRUD degli ingredienti create read e update

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,67 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.Models.Form;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class IngredientController : Controller
+    {
+
+        PizzaDbContext Db { get; set; }
+
+        public IngredientController()
+        {
+            Db = new PizzaDbContext();
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Ingredient> ingredients = Db.Ingredients.ToList();
+            return View(ingredients);
+        }
+
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Ingredient ingredient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Db.Add(ingredient);
+            Db.SaveChanges();
+
+            return RedirectToAction("Index");
+            
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Ingredient ingredient = Db.Ingredients.Find(id);
+
+
+            return View(ingredient);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Ingredient ingredient)
+        {
+            ingredient.Id = id;
+
+            Db.Update(ingredient);
+            Db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
